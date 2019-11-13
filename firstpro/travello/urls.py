f6c69d6e0acb408ab5c06e677e45b1d63d3f4f89
maheshhakeem <mahesh.hakeem916@gmail.com>
2019-11-13 09:22:29 +0530

    initial commit

---
+++ firstpro/travello/urls.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+# -*- coding: utf-8 -*-
+from django.urls import path
+from . import views
+
+urlpatterns = [
+        
+        path('', views.index, name='index'),
+        #path('add', views.add, name='add')
+        
+        ]
+
+
+
