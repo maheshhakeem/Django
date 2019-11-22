aec8ce8c9627c17c1e3f2f4cad704139b597bcd5
maheshhakeem <mahesh.hakeem916@gmail.com>
2019-11-22 20:56:51 +0530

    Passsing dynamic data by creating models & objects

--- firstpro/travello/views.py
+++ firstpro/travello/views.py
@@ -1,5 +1,27 @@
 from django.shortcuts import render
+from .models import Destination
 # Create your views here.
 
 def index(request):
-    return render(request,'index.html')
+    
+    dest1=Destination()
+    dest1.name="Mumbai"
+    dest1.img="destination_1.jpg"
+    dest1.price=123
+    dest1.desc="The City with More Population"
+    
+    dest2=Destination()
+    dest2.name="Hyderabad"
+    dest2.img='destination_2.jpg'
+    dest2.price=321
+    dest2.desc="The City of Bangles"
+    
+    dest3=Destination()
+    dest3.name="Banglore"
+    dest3.img='destination_3.jpg'
+    dest3.price=456
+    dest3.desc="The City of Plants"
+    
+    Dests=[dest1,dest2,dest3]
+    
+    return render(request,'index.html', {'Destinations':Dests})
