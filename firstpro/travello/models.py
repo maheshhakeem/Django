d854449645da297070678824701d9a240e2d19e1
maheshhakeem <mahesh.hakeem916@gmail.com>
2019-11-24 19:25:51 +0530

    model creation & migration

--- firstpro/travello/models.py
+++ firstpro/travello/models.py
@@ -2,10 +2,11 @@ from django.db import models
 
 # Create your models here.
 
-class Destination:
-    id:int
-    name:str
-    img:str
-    price:int
-    desc:str
-    specialoffer:bool
+class Destination(models.Model):
+    
+    name = models.CharField(max_length=100)
+    price = models.IntegerField()
+    specialoffer = models.BooleanField(default=False)
+    desc = models.TextField()
+    img = models.ImageField(upload_to='pics')
+    
