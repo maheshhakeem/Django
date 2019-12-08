09d9963d3a7340b0c288ada4a2930839ef5fffb7
maheshhakeem <mahesh.hakeem916@gmail.com>
2019-12-08 20:59:50 +0530

    Passing Message Info

--- firstpro/accounts/views.py
+++ firstpro/accounts/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.contrib.auth.models import User, auth
+from django.contrib import messages
 # Create your views here.
 
 def register(request):
@@ -14,15 +15,21 @@ def register(request):
         Password2 = request.POST["Confirm Password"]
         if Password1 == Password2:
             if User.objects.filter(username = User_name).exists():
-                print("user already exists")
+                #print("user already exists")
+                messages.info(request, "user already exists")
+                return render(request, 'register.html')
             elif User.objects.filter(email = Email).exists():
-                print("email already exists")
+                #print("email already exists")
+                messages.info(request, "email already exists")
+                return render(request, 'register.html')
             else:
                 user = User.objects.create_user(first_name=First_name, last_name=Last_name, username=User_name,email=Email,password=Password1)
                 user.save();
-                print("user_created")      
+                messages.info(request, "user created")
+                return render(request, 'register.html')     
         else:
-            print("password doesn't match")
+           messages.info(request, "Password Not Matching")
+           return render(request, 'register.html')
         return redirect('/')        
     else:
         return render(request,'register.html')
